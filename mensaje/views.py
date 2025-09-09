b73ec3e94e8e30a7f1838fc88378ec502f2f1683
AmadorAyalaAngel4-2 <a24311010@uthermosillo.edu.mx>
2025-09-09 07:34:25 -0700

    Cambiar texto en view.py
    mi segundo commit

--- mensaje/views.py
+++ mensaje/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 def saludo(request):
-    return HttpResponse("Hola Soy Angel Amador desde Django, listo para PythonAnywhere!")
+    return HttpResponse("Hola Soy Angel Amador desde Django, este es mi primer proyecto")
